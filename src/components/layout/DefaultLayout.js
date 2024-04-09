87f72886cc2c161320f24d52e518fa2961fb844c
yueayase <racham.chen@gmail.com>
2024-04-09 23:35:30 +0800

    update header

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -4,7 +4,15 @@ import Header from './Header';
 import Footer from './Footer';
 import Container from '../common/Comtainer';
 
-const PageHeader = styled.div`
+// const PageHeader = styled.div`
+//     ${(props) => 
+//     props.fixed && 
+//     css`
+//         position: fixed;
+//     `}
+// `;
+
+const PageHeader = styled(Header)`
     ${(props) => 
     props.fixed && 
     css`
@@ -15,9 +23,10 @@ const PageHeader = styled.div`
 const DefaultLayout = ({fixedHeader, children}) => {
     return (
         <div>
-            <PageHeader fixed={fixedHeader}>
+            {/* <PageHeader fixed={fixedHeader}>
                 <Header />
-            </PageHeader>
+            </PageHeader> */}
+            <PageHeader />
             <Container>{children}</Container>
             <Footer />
         </div>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,80 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Input } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 import Container from '../common/Comtainer';
+import Logo from '../images/logo.png'
 
 const StyledHeader = styled.header`
     background-color: #d0011b;
     width: 100vw;
-    height: 200px;
+    padding: 16px 0px;
 `;
+
+const StyledHeaderSection = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Navigator = styled.div`
+    a {
+        margin: 0px 4px;
+        color: white;
+        text-decoration: none;
+    }
+`;
+
+const Toolbar = styled.div`
+    a {
+        margin: 0px 6px;
+        color: white;
+        text-decoration: none;
+    }
+`;
+
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const { Search } = Input;
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 const Header = () => {
     return (
         <StyledHeader>
             <Container>
-            This is a fixed header
+                <StyledHeaderSection>
+                    <Navigator>
+                        <a href="#!">蝦皮購物</a>
+                        <a href="#!">下載</a>
+                        <a href="#!">追蹤我們</a>
+                        <a href="#!">部落格</a>
+                    </Navigator>
+                    <Toolbar>
+                        <a href="#!">通知</a>
+                        <a href="#!">幫助中心</a>
+                        <a href="#!">帳號</a>
+                    </Toolbar>
+                </StyledHeaderSection>
+                <StyledHeaderSection>
+                    <Link to="/">
+                        <img src={Logo} alt="logo" height={36}></img>
+                    </Link>
+                    <Flex>
+                        <Search 
+                            style={{ marginRight: 8 }}
+                            placeholder="在商城搜尋" 
+                            onSearch={onSearch} 
+                            enterButton 
+                        />
+                        <Link to="/cart">
+                            <ShoppingCartOutlined style={{ fontSize: '32px', color: 'white' }}/>
+                        </Link>
+                    </Flex>
+                </StyledHeaderSection>
             </Container>
         </StyledHeader>
     );
